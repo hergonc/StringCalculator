0bf8f53aec63e7424e5776bc9c49e10a5784da8c
Oscar Herrero <oherrero@aidacanarias.com>
2021-11-04 00:05:28 +0100

    Refactoring

--- StringCalculator/Calculator.cs
+++ StringCalculator/Calculator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace StringCalculator
@@ -8,19 +9,18 @@ namespace StringCalculator
         public static int Add(string values)
         {
             if (string.IsNullOrEmpty(values)) return 0;
+            List<string> delimiter = new List<string>(){",", "\n"};
             if (values.StartsWith("//"))
             {
-                string delimiter = values.Substring(2, 1);
-                values = values.Substring(4).Replace(delimiter, ",");
+                delimiter.Add(values.Substring(2, 1));
+                values = values.Substring(4);
             }
-            if (values.Contains("\n")) values = values.Replace("\n", ",");
-            int result = values.Split(',').Where(n => int.Parse(n) >= 0 && int.Parse(n) <= 1000).Sum(int.Parse);
-            if (values.Split(',').Any(n => int.Parse((string)n) < 0))
+            if (values.Split(delimiter.ToArray(), StringSplitOptions.RemoveEmptyEntries).Any(n => int.Parse((string)n) < 0))
                 throw new ArgumentException("negatives not allowed",
                     values.Split(',').Where(n => int.Parse(n) < 0)
                                 .Select(n => $"{n}")
                                 .Aggregate((exc, n) => exc + "," + n));
-            return result;
+            return values.Split(delimiter.ToArray(), StringSplitOptions.RemoveEmptyEntries).Where(n => int.Parse(n) >= 0 && int.Parse(n) <= 1000).Sum(int.Parse);
         }
     }
 }
