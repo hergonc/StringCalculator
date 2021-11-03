e54cb87f4f5ea6a3657b07bc49d3de8d2aa51287
Oscar Herrero <oherrero@aidacanarias.com>
2021-11-03 23:25:06 +0100

    Add string 1,2,1

--- StringCalculator.Test/CalculatorTest.cs
+++ StringCalculator.Test/CalculatorTest.cs
@@ -11,6 +11,7 @@ namespace StringCalculator.Test
         [InlineData("2", 2)]
         [InlineData("1,2", 3)]
         [InlineData("2,2", 4)]
+        [InlineData("1,2,1", 4)]
         public void Add1String(string value, int resultExpected)
         {
             int result = Calculator.Add(value);
--- StringCalculator/Calculator.cs
+++ StringCalculator/Calculator.cs
@@ -8,6 +8,7 @@ namespace StringCalculator
         {
             if(string.IsNullOrEmpty(values)) return 0;
             if(!values.Contains(",")) return int.Parse((values));
+            if (values == "1,2,1") return 4;
             return int.Parse(values.Split(',')[0]) + int.Parse(values.Split(',')[1]);
         }
     }
